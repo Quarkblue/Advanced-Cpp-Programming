fc4e13d5aa391455cc07ce35cc7c48acab343251
Divyansh Chauhan(Quark Blue) <d.s.c.r.3012@gmail.com>
2022-07-27 22:53:02 +0530

    .

---
+++ Exceptions/Custom-Exceptions.cpp
@@ -0,0 +1,37 @@
+// Learn Advance C++ programming
+// Custom Exceptions
+
+#include <iostream>
+#include <exception>
+using namespace std;
+
+
+class MyException : public exception {
+	
+public:
+	virtual const char* what() const throw() {
+		return "Something bad happened";
+	}
+	
+};
+
+class Test {
+public:
+	void goesWrong() {
+		throw MyException();
+	}
+};
+
+int main() {
+
+	Test test;
+	
+	try {
+		test.goesWrong();
+	}
+	catch (MyException& e) {
+		cout << e.what() << endl;
+	}
+
+	return 0;
+}
