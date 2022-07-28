4e9ff950e5360131c25dea84afba295f7bedd4d5
Divyansh Chauhan(Quark Blue) <d.s.c.r.3012@gmail.com>
2022-07-28 23:27:42 +0530

    .

--- Files/Reading-text-files.cpp
+++ Files/Reading-text-files.cpp
@@ -2,11 +2,32 @@
 // Reading Text files
 
 #include<iostream>
+#include<fstream>
+#include<string>
 using namespace std;
 
 int main() {
 	
+	string inFileName = "text.txt";
+	ifstream inFile;
+	
+	inFile.open(inFileName);
 
+	if (inFile.is_open()) {
+		
+		string line;
+		
+		while (inFile) {
+			getline(inFile, line);
+
+			cout << line << endl;
+		}
+		inFile.close();
+	}
+	else
+	{
+		cout << "Cannot open file :" << inFileName << endl;
+	}
 
 	return 0;
 }
