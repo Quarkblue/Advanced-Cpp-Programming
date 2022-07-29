854e4ee41ed58843bf2fa8201e925a5508f8bf92
Divyansh Chauhan(Quark Blue) <d.s.c.r.3012@gmail.com>
2022-07-30 00:18:39 +0530

    .

--- Files/Structs-and-Padding.cpp
+++ Files/Structs-and-Padding.cpp
@@ -1,22 +1,39 @@
 // Learn Advance C++ Programming
-// Structs and Padding
+// Structs and Padding, Reading and writing binary files
 
 #include<iostream>
 #include<fstream>
 #include<string>
 using namespace std;
 
-
 #pragma pack(push,1)
 struct Person {
 	char name[50];
 	int age;
-	double weight;
+	double height;
 };
+#pragma pop()
 
 int main() {
 	
-	cout << sizeof(Person) << endl;
+	Person someone = { "Frodo",220,0.8 };
+
+	string filename = "test.bin";
+	
+	ofstream outfile;
+	
+	outfile.open(filename, ios::binary);
+
+	if (outfile.is_open()) {
+
+		outfile.write(reinterpret_cast<char *>(&someone), sizeof(someone));
+
+		outfile.close();
+	}
+	else
+	{
+		cout << "Could not create file: " + filename;
+	}
 	
 	return 0;
 }
