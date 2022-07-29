48ae764faade2486b4b4c98da263087dd2f827a3
Divyansh Chauhan(Quark Blue) <d.s.c.r.3012@gmail.com>
2022-07-29 20:33:05 +0530

    .

--- Files/Parsing-text-files.cpp
+++ Files/Parsing-text-files.cpp
@@ -8,7 +8,30 @@ using namespace std;
 
 int main() {
 
+	string filename = "stats.txt";
+	ifstream input;
+	
+	input.open(filename);
+	
+	if (!input.is_open()) {
+		return 1;
+	}
+	
+	while (input) {
+		string line;		
+		getline(input, line, ':');
+		
+		int population;
+		input >> population;
+		
+		input >> ws;
+		
 
+		cout << line << " -- " << population << endl;
+
+	}
+
+	input.close();
 
 	return 0;
 }
