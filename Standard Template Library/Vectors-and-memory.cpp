a78baaa9423718574ecec34b6f28f2d0bf10d418
Divyansh Chauhan(Quark Blue) <d.s.c.r.3012@gmail.com>
2022-07-31 02:51:57 +0530

    .

--- Standard Template Library/Vectors-and-memory.cpp
+++ Standard Template Library/Vectors-and-memory.cpp
@@ -9,6 +9,28 @@ using namespace std;
 
 int main() {
 
+	vector<double> numbers(0);
+	
+	cout << "Size: " << numbers.size() << endl;
+
+	int capacity = numbers.capacity();
+	cout << "Capactity: " << capacity << endl;
+	for (int i = 0; i < 10000; i++)
+	{
+		if (numbers.capacity() != capacity) {
+			capacity = numbers.capacity();
+			cout << "Capactity: " << capacity << endl;
+		}
+		
+		numbers.push_back(i);
+		
+	}
+	
+	numbers.resize(100);
+	cout << numbers[2] << endl;
+	cout << "Size: " << numbers.size() << endl;
+	cout << "Capactity: " << capacity << endl;
+
 
 	return 0;
 }
