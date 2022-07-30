1174aa47e6dec7f701da6ee4e77793a721bdbc1b
Divyansh Chauhan(Quark Blue) <d.s.c.r.3012@gmail.com>
2022-07-30 12:06:28 +0530

    .

--- Standard Template Library/Vectors.cpp
+++ Standard Template Library/Vectors.cpp
@@ -2,10 +2,32 @@
 // Vectors
 
 #include<iostream>
+#include<vector>
 using namespace std;
 
 int main() {
-
 	
+	vector<string> strings;
+
+	strings.push_back("one");
+	strings.push_back("two");
+	strings.push_back("three");
+	
+	
+	///ways of accessing elements in vector
+	
+
+	/*for (int i = 0; i < strings.size(); i++) {
+		
+		cout << strings[i] << endl;
+	}*/
+	
+	//vector<string>::iterator it = strings.begin();
+	
+	/*for (vector<string>::iterator it = strings.begin(); it != strings.end(); it++) {
+		cout << *it << endl;
+	}*/
+	
+
 	return 0;
 }
