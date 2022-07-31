0d72f240c6ca50f33a0c9e00d0a17fde6bba07b4
Divyansh Chauhan(Quark Blue) <d.s.c.r.3012@gmail.com>
2022-07-31 23:38:03 +0530

    Update 2Dim-Vectors.cpp

--- Standard Template Library/2Dim-Vectors.cpp
+++ Standard Template Library/2Dim-Vectors.cpp
@@ -8,6 +8,14 @@ using namespace std;
 
 int main() {
 	
+	vector< vector<int> > grid(3,vector<int>(4,7));
+	
+	for (int row = 0; row < grid.size(); row++) {
+		for (int col = 0; col < grid[row].size(); col++) {
+			cout << grid[row][col] << flush;
+		}
+		cout << endl;
+	}
 	
 	return 0;
 }
