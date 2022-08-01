f493c892ec3e4bdd0be0277e9fc5a8565cfcaf72
Divyansh Chauhan(Quark Blue) <d.s.c.r.3012@gmail.com>
2022-08-01 22:46:31 +0530

    .

--- Standard Template Library/maps.cpp
+++ Standard Template Library/maps.cpp
@@ -2,13 +2,36 @@
 // Maps
 
 #include<iostream>
+#include<map>
+#include<string>
 
 using namespace std;
 
 
 int main() {
 
+	map<string, int> ages;
 
+	ages["john"] = 40;
+	ages["raj"] = 20;
+	ages["vicki"] = 30;
+	
+	
+
+	cout << ages["raj"] << endl;
+	
+	if (ages.find("lol") != ages.end()) {
+		cout << "Found lol" << endl;
+	}
+	else {
+		cout << "Key not found" << endl;
+	}
+
+	for (map<string, int>::iterator it = ages.begin(); it != ages.end(); it++) {
+
+		cout << it->first << " : " << it->second << endl;
+		
+	}
 
 	return 0;
 }
