ddf5959e7bff81586ee45bd8b72ba6711e15c3be
Divyansh Chauhan(Quark Blue) <d.s.c.r.3012@gmail.com>
2022-08-01 23:00:05 +0530

    Create Custom-Objects-As-Map-values.cpp

---
+++ Standard Template Library/Custom-Objects-As-Map-values.cpp
@@ -0,0 +1,36 @@
+// Learn Advance C++ Programming
+// Custom Objects as Map Values
+
+#include<iostream>
+#include<map>
+#include<String>
+
+using namespace std;
+
+class Person {
+private:
+	string name;
+	int age;
+	
+public:
+	
+	Person(string name, int age) :
+		name(name), age(age) {
+		
+	}
+
+	void print() const {
+		cout << name << " : " << age << endl;
+	}
+};
+
+
+int main() {
+	
+	map<int, Person> people;
+	
+	people[0] = Person("Mike", 40);
+	people[1] = Person("John", 30);
+	people[2] = Person("Mary". 25);
+	
+}
