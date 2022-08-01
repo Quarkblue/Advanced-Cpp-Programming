5ed7b4b059967ade709089ae9c81104a818fb623
Divyansh Chauhan(Quark Blue) <d.s.c.r.3012@gmail.com>
2022-08-01 15:56:10 +0530

    .

--- Standard Template Library/Lists.cpp
+++ Standard Template Library/Lists.cpp
@@ -2,12 +2,47 @@
 // Lists
 
 #include<iostream>
+#include<list>
 
 using namespace std;
 
 int main() {
 
+	list<int> numbers;
+	
+	numbers.push_back(1);
+	numbers.push_back(2);
+	numbers.push_back(3);
+	numbers.push_front(0);
 
+	list<int>::iterator it = numbers.begin();
+	it++;
+	numbers.insert(it, 100);
+	cout << "Element: " << *it << endl;
+	
+	list<int>::iterator eraseIt = numbers.begin();
+	eraseIt++;
+	eraseIt = numbers.erase(eraseIt);
+	cout << "Element after erasing: " << *eraseIt << endl;
+
+	for (list<int>::iterator it = numbers.begin(); it != numbers.end();) {
+		if (*it == 2) {
+			numbers.insert(it, 1234);
+		}
+		if (*it == 1) {
+			it = numbers.erase(it);
+		}
+		else {
+			it++;
+		}
+		
+	}
+
+	
+	for (list<int>::iterator it = numbers.begin(); it != numbers.end(); it++) {
+		
+		cout << *it << endl;
+	}
 	
 	return 0;
 }
