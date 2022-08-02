dc05bcaa20f5328723a1cea9327939c7c1ba9b61
Divyansh Chauhan(Quark Blue) <d.s.c.r.3012@gmail.com>
2022-08-02 23:53:43 +0530

    .

--- Standard Template Library/MultiMaps.cpp
+++ Standard Template Library/MultiMaps.cpp
@@ -8,6 +8,12 @@ using namespace std;
 
 int main() {
 	
+	multimap<int, string> lookup;
+
+	lookup.insert(make_pair(30, "Mike"));
+	lookup.insert(make_pair(30, "Vicki"));
+	lookup.insert(make_pair(30, "Raj"));
+	lookup.insert(make_pair(30, "Bob"));
 
 	
 	return 0;
