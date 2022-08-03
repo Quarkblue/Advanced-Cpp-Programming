790fe2cb7924fd4eed71c9bc2d5d967524f9d69f
Divyansh Chauhan(Quark Blue) <d.s.c.r.3012@gmail.com>
2022-08-03 13:06:18 +0530

    Update MultiMaps.cpp

--- Standard Template Library/MultiMaps.cpp
+++ Standard Template Library/MultiMaps.cpp
@@ -11,10 +11,50 @@ int main() {
 	multimap<int, string> lookup;
 
 	lookup.insert(make_pair(30, "Mike"));
-	lookup.insert(make_pair(30, "Vicki"));
+	lookup.insert(make_pair(10, "Vicki"));
 	lookup.insert(make_pair(30, "Raj"));
-	lookup.insert(make_pair(30, "Bob"));
+	lookup.insert(make_pair(20, "Bob"));
 
+	// multimap stores values with the same key unlike normal map
+	// not storing same key values and over writing the string
+
+	for (multimap<int, string>::iterator it = lookup.begin(); it != lookup.end(); it++) {
+		
+		cout << it->first << " : " << it->second << endl;
+	}
+	
+	cout << endl;
+	
+	// use find in multimaps
+
+	for (multimap<int, string>::iterator it = lookup.find(20); it != lookup.end(); it++) {
+		
+		cout << it->first << " : " << it->second << endl;
+		
+	}
+
+	cout << endl;
+	
+	// Iterate through a range of values with the same key
+
+	pair<multimap<int, string>::iterator, multimap<int, string>::iterator> its = lookup.equal_range(30);
+	
+	for (multimap<int, string>::iterator it = its.first; it != its.second; it++) {
+		
+		cout << it->first << " : " << it->second << endl;
+
+	}
+
+	cout << endl;
+	
+	// A really short way to iterate through a range of values with the same key
+
+	auto  its2 = lookup.equal_range(30);
+	
+	for (multimap<int, string>::iterator it = its2.first; it != its2.second; it++) {
+		
+		cout << it->first << " : " << it->second << endl;
+	}
 	
 	return 0;
 }
