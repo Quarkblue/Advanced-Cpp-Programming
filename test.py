1a34a8b8c8b1550c8182a043c480a67d19234629
Divyansh Chauhan(Quark Blue) <d.s.c.r.3012@gmail.com>
2022-08-04 00:00:30 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,19 @@
+# data types
+
+x = 5 #int
+y = 'a' # char
+y = "hello" # string
+z = 4.3 #float
+
+
+
+# data structures
+
+
+lst = [[1,2,3], [4,5,6], [7,8,9]]
+tupple = (1,2,3)
+dog = {"tommy": 2, "fido": 1}
+
+
+lst2 = [dog,tupple]
+
