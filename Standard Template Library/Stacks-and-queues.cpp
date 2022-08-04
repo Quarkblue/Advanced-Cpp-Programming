5efd53d9ea686162d512a6e1bba729b300a747d1
Divyansh Chauhan(Quark Blue) <d.s.c.r.3012@gmail.com>
2022-08-04 23:58:56 +0530

    Update Stacks-and-queues.cpp

--- Standard Template Library/Stacks-and-queues.cpp
+++ Standard Template Library/Stacks-and-queues.cpp
@@ -7,7 +7,22 @@
 
 using namespace std;
 
+class Test {
+private:
+	string name;
 
+public:
+	Test(string name):
+		name(name) {
+			
+		}
+	~Test() {
+		// cout << "Object destroyed" << endl;
+	}
+	void print() {
+		cout << name << endl;
+	}
+};
 
 int main() {
 	
