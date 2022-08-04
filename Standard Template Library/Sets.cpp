3d329bceb6acc1a4299699e934a8adf7467b4b57
Divyansh Chauhan(Quark Blue) <d.s.c.r.3012@gmail.com>
2022-08-04 19:09:08 +0530

    Update Sets.cpp

--- Standard Template Library/Sets.cpp
+++ Standard Template Library/Sets.cpp
@@ -10,6 +10,26 @@ int main() {
 	
 	set<int> numbers;
 	
+	numbers.insert(50);
+	numbers.insert(30);
+	numbers.insert(20);
+	numbers.insert(10);
+	numbers.insert(20);
+
+	for (set<int>::iterator it = numbers.begin(); it != numbers.end(); it++) {
+		cout << *it << endl;
+	}
+	
+	set<int>::iterator itFind = numbers.find(10);
+
+	if (itFind != numbers.end()) {
+		cout << "Found: " << *itFind << endl;
+	}
+	
+	if (numbers.count(30)) {
+		cout << "Found: " << 30 << endl;
+	}
+
 	
 	
 	return 0;
