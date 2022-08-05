d9bb61a725727c8ca474321b462288a2f0b1a337
Divyansh Chauhan(Quark Blue) <d.s.c.r.3012@gmail.com>
2022-08-05 22:29:07 +0530

    .

--- Standard Template Library/STL-complex-datatypes.cpp
+++ Standard Template Library/STL-complex-datatypes.cpp
@@ -9,7 +9,24 @@ using namespace std;
 
 int main() {
 
+	map< string, vector<int> > scores;
 	
+	scores["mike"].push_back(10);
+	scores["mike"].push_back(20);
+	scores["vicky"].push_back(15);
+
+	for (map<string, vector<int> >::iterator it = scores.begin(); it != scores.end(); it++) {
+		string name = it->first;
+		vector<int> scoresList = it->second;
+		
+		cout << name << " : " << flush;
+		
+		for (int i = 0; i < scoresList.size(); i++) {
+			cout << scoresList[i] << " " << flush;
+		}
+
+		cout << endl;
+	}
 	
 	return 0;
 }
