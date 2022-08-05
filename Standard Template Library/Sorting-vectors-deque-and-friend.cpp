5bfed9375990126c8dc9aebd8f6ada5b2b908e44
Divyansh Chauhan(Quark Blue) <d.s.c.r.3012@gmail.com>
2022-08-05 20:27:13 +0530

    Update Sorting-vectors-deque-and-friend.cpp

--- Standard Template Library/Sorting-vectors-deque-and-friend.cpp
+++ Standard Template Library/Sorting-vectors-deque-and-friend.cpp
@@ -6,11 +6,30 @@
 
 using namespace std;
 
+class Test {
+	int id;
+	string name;
+	
+public:
+	Test(int id, string name) : id(id), name(name) {}
+	void print() {
+		cout << id << " : " << name << endl;
+	}
+};
 
 
 int main() {
 
+	vector<Test> tests;
+	
+	tests.push_back(Test(5, "Mike"));
+	tests.push_back(Test(10, "Sue"));
+	tests.push_back(Test(7, "Raj"));
+	tests.push_back(Test(3, "vicky"));
 
+	for (int i = 0; i < tests.size(); i++){
+		tests[i].print();
+	}
 
 	return 0;
 }
