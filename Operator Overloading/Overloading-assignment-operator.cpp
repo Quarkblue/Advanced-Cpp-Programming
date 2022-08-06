50ddef145dee2dae277fd94b110f9ef1df89cf40
Divyansh Chauhan(Quark Blue) <d.s.c.r.3012@gmail.com>
2022-08-07 00:24:43 +0530

    .

--- Operator Overloading/Overloading-assignment-operator.cpp
+++ Operator Overloading/Overloading-assignment-operator.cpp
@@ -18,12 +18,31 @@ public:
 	Test(int id, string name): id(id), name(name) {
 		
 	}
-	void print() {
+	void print() const {
 		cout << id << " : " << name << endl;
 	}
+	
+	const Test& operator=(const Test& other) {
+		
+		cout << "Assignment running" << endl;
+		
+		id = other.id;
+		name = other.name;
+		
+		return *this;
+	}
+	
 };
 
 int main() {
+	
+	Test test1(10, "Mike");
+	test1.print();
+
+	Test test2(20, "Bob");
+	
+	test2 = test1;
+	test2.print();
 	
 	return 0;
 }
