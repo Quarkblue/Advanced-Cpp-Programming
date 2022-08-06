dcd3706f3f3ec42a3a8f3ef63a2abbfb57616798
Divyansh Chauhan(Quark Blue) <d.s.c.r.3012@gmail.com>
2022-08-06 23:00:52 +0530

    Update Overloading-assignment-operator.cpp

--- Operator Overloading/Overloading-assignment-operator.cpp
+++ Operator Overloading/Overloading-assignment-operator.cpp
@@ -3,8 +3,25 @@
 
 #include<iostream>
 
-
 using namespace std;
+
+class Test {
+	
+private:
+	int id;
+	string name;
+	
+public:
+	Test() : id(0), name("") {
+		
+	}
+	Test(int id, string name): id(id), name(name) {
+		
+	}
+	void print() {
+		cout << id << " : " << name << endl;
+	}
+};
 
 int main() {
 	
