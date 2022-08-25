f2452286d5507c756e2a5e8cd42b2f14d2ed8537
Divyansh Chauhan(Quark Blue) <d.s.c.r.3012@gmail.com>
2022-08-26 01:49:39 +0530

    .

--- Passing functions to functions/Functors.cpp
+++ Passing functions to functions/Functors.cpp
@@ -5,10 +5,39 @@
 
 using namespace std;
 
+struct Test {
+	virtual bool operator()(string &text) = 0;
+	
+	virtual ~Test() {};
+
+};
+
+struct MatchTest: public Test
+{
+	bool operator()(string &text) {
+		return text == "lion";
+	}
+};
+
+void check(string &text, Test &test) {
+	if (test(text)) {
+		cout << "Text match" << endl;
+	}
+	else {
+		cout << "No match" << endl;
+	}
+}
+
 
 int main() {
 
+	MatchTest pred;
+	
+	string value = "lion";
+	
+	MatchTest n;
 
+	check(value, n);
 	
 	return 0;
 }
