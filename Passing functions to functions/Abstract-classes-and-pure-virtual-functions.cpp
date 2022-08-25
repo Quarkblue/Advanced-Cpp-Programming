bdecddcd30c82382cdd1434b61ae7e9c58f9ef7e
Divyansh Chauhan(Quark Blue) <d.s.c.r.3012@gmail.com>
2022-08-25 21:33:48 +0530

    .

--- Passing functions to functions/Abstract-classes-and-pure-virtual-functions.cpp
+++ Passing functions to functions/Abstract-classes-and-pure-virtual-functions.cpp
@@ -5,8 +5,27 @@
 
 using namespace std;
 
+
+// A class that contains pure virtual functions is called an Abstract class
+class Animal {
+public:
+	virtual void speak() = 0; // A pure virtual function
+	
+	
+};
+
+class Dog: public Animal {
+public:
+	virtual void speak() {
+		cout << "Woof!" << endl;
+	}
+};
+
+
 int main() {
 	
+	Dog dog;
+	dog.speak();
 
 	return 0;
 }
