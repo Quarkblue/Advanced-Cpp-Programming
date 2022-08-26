aa4f1665aba6e397cc696a31c3e060d5d5c0f448
Divyansh Chauhan(Quark Blue) <d.s.c.r.3012@gmail.com>
2022-08-27 02:03:15 +0530

    .

--- Cpp'11 New features/Auto-keyword.cpp
+++ Cpp'11 New features/Auto-keyword.cpp
@@ -5,9 +5,29 @@
 
 using namespace std;
 
+template<class T, class S>
+auto test(T value, S value2) -> decltype(value + value2) {
+
+	return value + value2;
+}
+
+int get() {
+	return 999;
+}
+
+auto test2() -> decltype(get()) {
+	return get();
+}
+
 int main() {
 
+	auto value = 7;
+	auto text = "Hello";
 
+	cout << test(5,6) << endl;
+
+	cout << test2() << endl;
+	
 	return 0;
 }
 
