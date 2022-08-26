4aed648e46208b13399942be4120bb77fc162ddc
Divyansh Chauhan(Quark Blue) <d.s.c.r.3012@gmail.com>
2022-08-27 01:39:41 +0530

    .

--- Cpp'11 New features/Dcltype-typeid-nameMangeling.cpp
+++ Cpp'11 New features/Dcltype-typeid-nameMangeling.cpp
@@ -1,14 +1,20 @@
 // Learn Advance CPP Programming
 // Decltype, Typeid and Name Mangling
 
-
 #include<iostream>
+#include<typeinfo>
 
 using namespace std;
 
 int main() {
 	
+	string value;
+	
+	cout << typeid(value).name() << endl;
 
+	decltype(value) name = "Bob";
+	
+	cout << typeid(name).name() << endl;
 	
 	return 0;
 }
