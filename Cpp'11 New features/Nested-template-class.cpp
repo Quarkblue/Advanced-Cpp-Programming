773535d6f380dbbeee35f6b7489a22a9b40188ba
Divyansh Chauhan(Quark Blue) <d.s.c.r.3012@gmail.com>
2022-08-28 23:10:41 +0530

    .

---
+++ Cpp'11 New features/Nested-template-class.cpp
@@ -0,0 +1,13 @@
+// Learn Advance C++ Programming
+// Nested Template classes
+
+#include<iostream>
+
+using namespace std;
+
+
+int main() {
+
+	return 0;
+}
+
---
+++ Ring.h
@@ -0,0 +1,12 @@
+#pragma once
+#ifndef RING_H_
+#define RING_H_
+
+class Ring
+{
+public:
+	Ring();
+	virtual ~Ring();
+};
+
+#endif // !RING_H_
