6a73b6a5e4eb7d95bdc9fcf09f4efe6dbb869e51
Divyansh Chauhan(Quark Blue) <d.s.c.r.3012@gmail.com>
2022-08-28 23:02:32 +0530

    .

--- Cpp'11 New features/range-based-loops.cpp
+++ Cpp'11 New features/range-based-loops.cpp
@@ -2,11 +2,26 @@
 // Range Based loops
 
 #include <iostream>
+#include<vector>
 
 using namespace std;
 
 int main() {
 
+	auto texts = {"one","two","three"};
+
+	for (auto text : texts) {
+		cout << text << endl;
+	}
+
+	vector<int> numbers;
+	numbers.push_back(1);
+	numbers.push_back(2);
+	numbers.push_back(3);
+	
+	for (auto number : numbers) {
+		cout << number << endl;
+	}
 
 	return 0;
 }
