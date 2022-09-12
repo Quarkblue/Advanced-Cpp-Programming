af8935f174bfe05ba9bf78177a5262c8a602cabb
Divyansh Chauhan(Quark Blue) <d.s.c.r.3012@gmail.com>
2022-09-13 00:41:17 +0530

    .

--- Cpp'11 New features/A-ring-buffer-class.cpp
+++ Cpp'11 New features/A-ring-buffer-class.cpp
@@ -6,7 +6,19 @@ using namespace std;
 
 int main() {
 
+	ring<string> textring(3);
+	
+	textring.add("one");
+	textring.add("two");
+	textring.add("three");
+	textring.add("four");
+
+	for (int i = 0; i < textring.size(); i++) {
+		cout << textring.get(i) << endl;
+	}
 
 
+	
 	return 0;
 }
+
--- Cpp'11 New features/Ring.h
+++ Cpp'11 New features/Ring.h
@@ -6,14 +6,47 @@
 using namespace std;
 
 template<class T>
-class Ring
+class ring
 {
+private:
+	int m_pos;
+	int m_size;
+	T* m_values;
+	
+	
 public:
 	class iterator;
+	
+public:
+	ring(int size) : m_pos(0), m_size(size), m_values(NULL) {
+		m_values = new T[size];
+	}
+	
+	~ring() {
+		delete [] m_values;
+	}
+
+	int size() {
+		return m_size;
+	}
+	
+	void add(T value) {
+		m_values[m_pos++] = value;
+		
+		if (m_pos == m_size) {
+			m_pos = 0;
+		}
+	}
+	
+	T& get(int pos) {
+		return m_values[pos];
+	}
+
 };
 
+
 template<class T>
-class Ring<T>::iterator {
+class ring<T>::iterator {
 public:
 	void print() {
 		cout << "hello iterator" << T() << endl;
