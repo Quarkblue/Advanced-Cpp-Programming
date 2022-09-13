36f1712fd9a0aea1e87d21f43add9b9dd0310e98
Divyansh Chauhan(Quark Blue) <d.s.c.r.3012@gmail.com>
2022-09-13 21:11:47 +0530

    Update A-ring-buffer-class.cpp

--- Cpp'11 New features/A-ring-buffer-class.cpp
+++ Cpp'11 New features/A-ring-buffer-class.cpp
@@ -12,6 +12,7 @@ int main() {
 	textring.add("two");
 	textring.add("three");
 	textring.add("four");
+	textring.add("five");
 
 	for (int i = 0; i < textring.size(); i++) {
 		cout << textring.get(i) << endl;
