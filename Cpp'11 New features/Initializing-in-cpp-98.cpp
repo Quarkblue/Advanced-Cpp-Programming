49e9c2a1cb2e7fd1e70b20cce699cf7dbe7a3ecf
Divyansh Chauhan(Quark Blue) <d.s.c.r.3012@gmail.com>
2022-09-15 20:39:26 +0530

    .

--- Cpp'11 New features/Initializing-in-cpp-98.cpp
+++ Cpp'11 New features/Initializing-in-cpp-98.cpp
@@ -4,7 +4,22 @@ using namespace std;
 
 int main() {
 	
+	int values[] = {1, 4, 5};
 	
+	class S1 {
+	public:
+		string text;
+		int id;
+	};
+	
+	S1 s1 = { "Hello", 7 };
+
+	struct C {
+		string text;
+		int id;
+	};
+	
+	C c1 = {"Hello", 7};
 	
 	return 0;
 }
