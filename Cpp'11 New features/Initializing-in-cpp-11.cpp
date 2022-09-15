47d78cf669439ad4b54319b5b6524f5de287da5a
Divyansh Chauhan(Quark Blue) <d.s.c.r.3012@gmail.com>
2022-09-15 21:13:47 +0530

    Update Initializing-in-cpp-11.cpp

--- Cpp'11 New features/Initializing-in-cpp-11.cpp
+++ Cpp'11 New features/Initializing-in-cpp-11.cpp
@@ -9,6 +9,14 @@ int main() {
 
 	cout << value << endl;
 	
+	int nums[]{ 1,2,4 };
+	
+	cout << nums[1] << endl;
+
+	int* pInts = new int[3] {1, 2, 3};
+	cout << pInts[1] << endl;
+	delete pInts;
+	
 	return 0;
 }
 
