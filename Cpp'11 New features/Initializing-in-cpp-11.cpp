df1dd99e0e325e4b7d3692baf2836c9d14ded8ba
Divyansh Chauhan(Quark Blue) <d.s.c.r.3012@gmail.com>
2022-09-15 21:21:57 +0530

    Update Initializing-in-cpp-11.cpp

--- Cpp'11 New features/Initializing-in-cpp-11.cpp
+++ Cpp'11 New features/Initializing-in-cpp-11.cpp
@@ -16,7 +16,25 @@ int main() {
 	int* pInts = new int[3] {1, 2, 3};
 	cout << pInts[1] << endl;
 	delete pInts;
+
+	int value1{};
+	cout << value1 << endl;
 	
+	int* plol{nullptr};   // Equivalent to int *plol = nullptr
+	
+	cout << plol << endl;
+	
+	struct {
+		int value;
+		string text;
+	}s1{5, "Hello"};
+	
+	cout << s1.text << endl;
+	
+	vector<string> strings{ "One", "Two", "Three" };
+
+	cout << strings[2] << endl;
+
 	return 0;
 }
 
