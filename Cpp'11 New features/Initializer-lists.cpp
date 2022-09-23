98d51aee4d7ac304636909c08805522ef3be1548
Divyansh Chauhan(Quark Blue) <d.s.c.r.3012@gmail.com>
2022-09-24 02:26:48 +0530

    .

--- Cpp'11 New features/Initializer-lists.cpp
+++ Cpp'11 New features/Initializer-lists.cpp
@@ -1,11 +1,30 @@
 #include<iostream>
 #include<vector>
+#include<initializer_list>
 
 using namespace std;
 
+class Test {
+public:
+	Test(initializer_list<string> texts) {
+		for (auto value : texts)
+			cout << value << endl;
+	}
+
+	void print(initializer_list<string> l) {
+		for (auto value : l) {
+			cout << value << endl;
+		}
+	}
+	
+};
+
 int main() {
 	
-	vector<int> numbers{ 1, 3, 4 6 };
+	vector<int> numbers{ 1, 3, 4, 6 };
+	cout << numbers[2] << endl;
+	
+	Test test{ "Hello", "how", "are", "you" };
 
 	return 0;
 }
