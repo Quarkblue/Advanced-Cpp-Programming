807cde96a854adcd58e5fa56aa8d0d4c8873766c
Divyansh Chauhan(Quark Blue) <d.s.c.r.3012@gmail.com>
2022-10-08 23:12:07 +0530

    .

--- Cpp'11 New features/Lambda-expression.cpp
+++ Cpp'11 New features/Lambda-expression.cpp
@@ -5,8 +5,21 @@
 
 using namespace std;
 
+void test(void(*pfunc)()) {
+	pfunc();
+}
+
 int main() {
 	
+	auto func = []() {cout << "Being called by a name" << endl; };
+
+	func();
+
+	[]() {cout << "Not being called by a name" << endl; }();
+
+	test(func);
 	
+	test([]() {cout << "Not being called by a name" << endl; });
+
 	return 0;
 }
