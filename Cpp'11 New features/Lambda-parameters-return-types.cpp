0d0570862dc47ba6fd3ebd39f8de2ca264aafda7
Divyansh Chauhan(Quark Blue) <d.s.c.r.3012@gmail.com>
2022-10-17 18:38:28 +0530

    .

--- Cpp'11 New features/Lambda-parameters-return-types.cpp
+++ Cpp'11 New features/Lambda-parameters-return-types.cpp
@@ -5,9 +5,33 @@
 
 using namespace std;
 
+void testGreet(void (*greet)(string)) {
+	greet("Bob");
+	
+}
+
+void runDivide(double (*divide)(double, double)) {
+	cout << divide(9, 3) << endl;
+}
 
 int main() {
 
+	auto pGreet = [](string name) {cout << "Hello " << name << endl; };
+	
+	pGreet("Mike");
+	
+	testGreet(pGreet);
+
+	auto pDivide = [](double a, double b) -> double {
+		if (b == 0) {
+			return 0;
+		}
+		return a / b;
+	};
+
+	cout << pDivide(10.0, 5.0) << endl;
+	
+	runDivide(pDivide);
 
 	return 0;
 }
