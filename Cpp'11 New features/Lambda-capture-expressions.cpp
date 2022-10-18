e17d6d69e9a9b638784ba239e6bc065c1c7f4cbb
Divyansh Chauhan(Quark Blue) <d.s.c.r.3012@gmail.com>
2022-10-18 17:19:21 +0530

    Update Lambda-capture-expressions.cpp

---
+++ Cpp'11 New features/Lambda-capture-expressions.cpp
@@ -0,0 +1,19 @@
+// Learn Advance C++ Programming
+// Lambda Capture Expression
+
+#include<iostream>
+
+using namespace std;
+
+
+int main() {
+
+	int one = 1;
+	int two = 2;
+	int three = 3;
+
+	[]() {};
+	
+	
+	return 0;
+}
