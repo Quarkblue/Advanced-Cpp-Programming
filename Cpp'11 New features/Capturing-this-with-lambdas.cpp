1e90481abfc10192566b5c44edb35cc737585ce5
Divyansh Chauhan(Quark Blue) <d.s.c.r.3012@gmail.com>
2022-10-18 17:32:49 +0530

    Update Capturing-this-with-lambdas.cpp

---
+++ Cpp'11 New features/Capturing-this-with-lambdas.cpp
@@ -0,0 +1,25 @@
+// Learn Advance C++ Programming
+// Capturing this with lambdas
+
+#include<iostream>
+
+using namespace std;
+
+class Test {
+private:
+	int one;
+	int two;
+	
+public:
+	void run() {
+		int three = 3;
+		int four = 4;
+	}
+};
+
+int main() {
+	Test test;
+	test.run();
+
+	return 0;
+}
