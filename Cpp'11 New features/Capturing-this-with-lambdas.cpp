0b8a5e24b0fd46d2266e9b54ccaec149c9d2991f
Divyansh Chauhan(Quark Blue) <d.s.c.r.3012@gmail.com>
2022-10-18 19:23:50 +0530

    .

--- Cpp'11 New features/Capturing-this-with-lambdas.cpp
+++ Cpp'11 New features/Capturing-this-with-lambdas.cpp
@@ -7,13 +7,24 @@ using namespace std;
 
 class Test {
 private:
-	int one;
-	int two;
+	int one{1};
+	int two{2};
 	
 public:
 	void run() {
-		int three = 3;
-		int four = 4;
+		int three{3};
+		int four{4};
+		
+		auto pLambda = [this, three, four]() {
+			one = 111;
+			two = 222;	
+			cout << one << endl;
+			cout << two << endl;
+			cout << three << endl;
+			cout << four << endl;
+		};
+		pLambda();
+		
 	}
 };
 
