b970788de726b01821cd8f079181f72b0d901f92
Divyansh Chauhan(Quark Blue) <d.s.c.r.3012@gmail.com>
2022-10-18 17:29:54 +0530

    .

--- Cpp'11 New features/Lambda-capture-expressions.cpp
+++ Cpp'11 New features/Lambda-capture-expressions.cpp
@@ -12,8 +12,18 @@ int main() {
 	int two = 2;
 	int three = 3;
 
-	[]() {};
+	// Capture one and two by values
+	[one, two]() {cout << one << "," << two << endl; }();
 	
+	// Capture all local variables by value
+	[=]() { cout << one << "," << two << "," << three << endl; }();
 	
+	// Capture all local variables by value, but capture three by reference
+	[=, &three]() { cout << one << "," << two << "," << three << endl; three = 7; }();
+	cout << three << endl;
+
+	// Capture all local variables by reference
+	[&]() {cout << two << endl; two = 8; cout << two << endl; }();
+
 	return 0;
 }
