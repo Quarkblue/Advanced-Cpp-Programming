d21f96a24b8ca0b1e85eba834da74f02bf791b6b
Divyansh Chauhan(Quark Blue) <d.s.c.r.3012@gmail.com>
2022-10-26 01:33:51 +0530

    Update Delegating-constructors.cpp

---
+++ Cpp'11 New features/Delegating-constructors.cpp
@@ -0,0 +1,43 @@
+// Learn Advance C++ Programming
+// Delegating Constructors
+
+#include<iostream>
+
+using namespace std;
+
+class Parent {
+
+	int dogs;
+	string text;
+	
+public:
+	Parent() {
+		dogs = 5;
+		cout << "No parameter parent constructor" << endl;
+	}
+	
+	Parent(string text) {
+		dogs = 5;
+		this->text = text;
+		cout << "String parent constructor" << endl;
+	}
+};
+
+class  Child : public Parent {
+
+public:
+	Child() {
+		
+	}
+	
+};
+
+
+int main() {
+
+	Parent parent;
+	Child child;
+	return 0;
+	
+}
+
