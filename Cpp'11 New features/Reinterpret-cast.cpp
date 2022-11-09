8d0de23e9532ce0a64795542ccd26992cbbbfcae
Divyansh Chauhan(Quark Blue) <d.s.c.r.3012@gmail.com>
2022-11-09 22:28:10 +0530

    Create Reinterpret-cast.cpp

---
+++ Cpp'11 New features/Reinterpret-cast.cpp
@@ -0,0 +1,40 @@
+// Learn Advanced C++ Programming
+// Reinterpret Casts
+
+#include<iostream>
+
+using namespace std;
+
+class Parent {
+	virtual void speak() {
+		cout << "parent!" << endl;
+	}
+};
+
+class Brother : public Parent {
+
+};
+
+class Sister : public Parent {
+
+};
+
+
+
+int main() {
+
+	Parent parent;
+	Brother brother;
+
+	Parent* pPBrother = &brother;
+
+	Brother* pBBrother = dynamic_cast<Brother*>(pPBrother);
+
+	if (pBBrother == nullptr) {
+		cout << "Invalid Cast" << endl;
+	}
+	else {
+		cout << pBBrother << endl;
+	}
+	return 0;
+}
