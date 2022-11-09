c6afa7bc9d045cc43b44bcc8f65a12a4575caf34
Divyansh Chauhan(Quark Blue) <d.s.c.r.3012@gmail.com>
2022-11-09 22:21:25 +0530

    Create Dynamic-casts.cpp

---
+++ Cpp'11 New features/Dynamic-casts.cpp
@@ -0,0 +1,47 @@
+// Learn Advanced C++ Programming
+// Dynamic Casts
+
+#include<iostream>
+
+using namespace std;
+
+class Parent {
+
+};
+
+class Brother : public Parent {
+	
+};
+
+class Sister : public Parent {
+	
+};
+
+
+
+int main() {
+
+	Parent parent;
+	Brother brother;
+
+	Parent* pParent = &brother; // Works
+
+	//Brother* pBrother = &parent // Does not work
+
+	// How to make it work?
+	// Unsafe btw
+	Brother* pBrother = static_cast<Brother*>(&parent);
+
+	Parent* pPBrother = &brother;
+	
+	Brother* pBBrother = static_cast<Brother *>(pPBrother);
+
+	cout << pPBrother << endl;
+
+	cout << pBrother << endl;
+	
+	Parent&& p = static_cast<Parent &&>(parent);
+
+	return 0;
+}
+
