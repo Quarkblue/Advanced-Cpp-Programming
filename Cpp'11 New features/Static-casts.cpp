66c36a17a0bef7f835fa1c5c8464caf5cfcb004a
Divyansh Chauhan(Quark Blue) <d.s.c.r.3012@gmail.com>
2022-11-09 22:08:12 +0530

    Create Static-casts.cpp

---
+++ Cpp'11 New features/Static-casts.cpp
@@ -0,0 +1,30 @@
+// Learn Advanced C++ Programming
+// Static Casts
+
+#include<iostream>
+
+using namespace std;
+
+class Parent {
+
+};
+
+class Brother : public Parent {
+	
+};
+
+class Sister : public Parent {
+	
+};
+
+
+
+int main() {
+
+	Parent parent;
+	Brother brother;
+
+
+	return 0;
+}
+
