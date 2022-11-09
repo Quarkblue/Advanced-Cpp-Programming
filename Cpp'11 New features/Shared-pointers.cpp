f9302b986b83831beb1bf77a70a555c2affe1ffb
Divyansh Chauhan(Quark Blue) <d.s.c.r.3012@gmail.com>
2022-11-10 00:08:22 +0530

    Update Shared-pointers.cpp

--- Cpp'11 New features/Shared-pointers.cpp
+++ Cpp'11 New features/Shared-pointers.cpp
@@ -24,9 +24,13 @@ public:
 
 int main() {
 	
-	shared_ptr<Test> pTest1 = make_shared<Test>();
-
+	shared_ptr<Test> pTest2(nullptr);
 	
+	{
+		shared_ptr<Test> pTest1 = make_shared<Test>();
+		pTest2 = pTest1;
+	}
+
 
 	cout << "Finished" << endl;
 	return 0;
