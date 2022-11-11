f64ec61900b2b2054605d0b0c3750faa3bbebcdc
Divyansh Chauhan(Quark Blue) <d.s.c.r.3012@gmail.com>
2022-11-11 13:52:19 +0530

    Update BitmapInfoHeader.h

--- Fractal Images/BitmapInfoHeader.h
+++ Fractal Images/BitmapInfoHeader.h
@@ -11,8 +11,8 @@ struct BitmapInfoHeader {
 	int32_t headerSize{ 40 };
 	int32_t width;
 	int32_t height;
-	int32_t planes{ 1 };
-	int32_t bitsPerPixel{ 24 };
+	int16_t planes{ 1 };
+	int16_t bitsPerPixel{ 24 };
 	int32_t compression{ 0 };
 	int32_t dataSize{ 0 };
 	int32_t horizontalResolution{ 2400 };
@@ -20,7 +20,5 @@ struct BitmapInfoHeader {
 	int32_t colors{ 0 };
 	int32_t importantColors{ 0 };
 };
-
-
 
 #endif /* BITMAPINFOHEADER_H_ */
