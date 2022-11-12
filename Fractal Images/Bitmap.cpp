09ac437ffebea352eaa36c31d3f8c33968be97cd
Divyansh Chauhan(Quark Blue) <d.s.c.r.3012@gmail.com>
2022-11-13 02:45:48 +0530

    .

--- Fractal Images/Bitmap.cpp
+++ Fractal Images/Bitmap.cpp
@@ -1,12 +1,26 @@
 #include "Bitmap.h"
+#include "BitmapFileHeader.h"
+#include "BitmapInfoHeader.h"
+
+using namespace fractal;
 
 namespace fractal {
 
-	Bitmap::Bitmap(int width, int height): m_width(width), m_height(height) {
+	Bitmap::Bitmap(int width, int height) : m_width(width), m_height(height), m_pPixel(new uint8_t[width * height * 3]{}) {
 		
 	}
 
 	bool Bitmap::write(string filename) {
+		BitmapFileHeader fileheader;
+		BitmapInfoHeader infoheader;
+			
+		fileheader.fileSize = sizeof(BitmapFileHeader) + sizeof(BitmapInfoHeader) + (m_width * m_height * 3);
+		fileheader.dataOffset = sizeof(BitmapFileHeader) + sizeof(BitmapInfoHeader);
+		
+		infoheader.width = m_width;
+		infoheader.height = m_height;
+		
+
 		return false;
 	}
 	void setPixel(int x, int y, uint8_t red, uint8_t green, uint8_t blue) {
--- Fractal Images/Bitmap.h
+++ Fractal Images/Bitmap.h
@@ -5,6 +5,7 @@
 
 #include<string>
 #include<cstdint>
+#include<memory>
 
 using namespace std;
 
@@ -14,6 +15,7 @@ namespace fractal {
 	private:
 		int m_width{0};
 		int m_height{0};
+		unique_ptr<uint8_t[]> m_pPixel{nullptr};
 	public:
 		Bitmap(int width, int height);
 		
