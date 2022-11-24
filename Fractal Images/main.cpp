68837e8dd964ac25473bb7b6b5ea838d1e6479f0
Divyansh Chauhan(Quark Blue) <d.s.c.r.3012@gmail.com>
2022-11-25 05:22:03 +0530

    .

--- Fractal Images/main.cpp
+++ Fractal Images/main.cpp
@@ -23,19 +23,19 @@ int main() {
 
 	for (int y = 0; y < HEIGHT; y++) {
 		for (int x = 0; x < WIDTH; x++) {
-			double xFractal{ (x - WIDTH/2) * (2.0/WIDTH) };
+			double xFractal{ (x - WIDTH/2 - 200) * (2.0/HEIGHT) };
 			double yFractal{ (y - HEIGHT / 2) * (2.0 / HEIGHT) };
 
 			int iterations = Mandelbrot::getIteration(xFractal, yFractal);
 
-			uint8_t red = (uint8_t)(256 * (double)iterations / Mandelbrot::MAX_ITERATIONS);
+			uint8_t color = (uint8_t)(256 * (double)iterations / Mandelbrot::MAX_ITERATIONS);
 
-
+			color = color * color * color; // it will range from 0 to 255 because uint8_t has the range from 0 to 255 only
 			
-			bitmap.setPixel(x, y, red, red, red);
+			bitmap.setPixel(x, y, 0, color, 0);
 			
-			if (red < min) min = red;
-			if (red > max) max = red;
+			if (color < min) min = color;
+			if (color > max) max = color;
 
 		}
 	}
